051eed92d7095c18ad326d627021f42f3ae863dd
Lívia Guimarães <liviafgn31@gmail.com>
2026-04-05 23:26:40 +0100

    Basic standard input and output fundamentals

--- Programming_lab3/question4.c
+++ Programming_lab3/question4.c
@@ -1,11 +1,11 @@
-/* Program description: calculation of sum and average of three float numbers, displaying results to 3 decimal places.
+/* Program description: calculation of sum, average, and percentage of float numbers, displaying results to specific decimal places.
 Author: Lívia Ferreira Guimarães Neves */
 
 #include <stdio.h>
 
 int main()
 {
-    float number_1, number_2, number_3, sum, average = 0; // ensure all variables are float numbers
+    float number_1, number_2, number_3, sum, average, percentage = 0; // ensure all variables are float numbers
 
     // variables being declared
     number_1 = printf("Enter first floating-point number: ");
@@ -16,11 +16,13 @@ int main()
     scanf("%f", &number_3);
 
     sum = number_1 + number_2 + number_3; // sum calculation
-    average = sum / 3; // average calculation
+    average = sum / 3; // average 
+    percentage = (number_1 / number_2) * 100; // percentage calculation
 
     // print of the results:
     printf("The sum of the three floating-point numbers is %.3f.", sum);
     printf("\nThe average of the three floating-point numbers is %.3f.", average);
+    printf("\nThe first number is %.1f%% of the second number.", percentage);
 
     return 0;
 }
