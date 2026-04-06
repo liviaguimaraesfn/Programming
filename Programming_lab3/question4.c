ef7eadaccfbe703296665bb584f9d14e76e04072
Lívia Guimarães <liviafgn31@gmail.com>
2026-04-06 23:50:04 +0100

    Basic standard input and output fundamentals

--- Programming_lab3/question4.c
+++ Programming_lab3/question4.c
@@ -1,11 +1,12 @@
-/* Program description: calculation of sum, average, and percentage of float numbers, displaying results to specific decimal places.
+/* Program description: calculation of sum, average, percentage, and division with remainder of float numbers, displaying results to specific decimal places.
 Author: Lívia Ferreira Guimarães Neves */
 
 #include <stdio.h>
 
 int main()
 {
-    float number_1, number_2, number_3, sum, average, percentage = 0; // ensure all variables are float numbers
+    float number_1, number_2, number_3, sum, average, percentage = 0; // initialising float numbers
+    int number_4, number_5, division, remainder; // initialising integer numbers
 
     // variables being declared
     printf("Enter first floating-point number: ");
@@ -14,15 +15,22 @@ int main()
     scanf("%f", &number_2);
     printf("Enter third floating-point number: ");
     scanf("%f", &number_3);
+    printf("Enter first integer number: ");
+    scanf("%d", &number_4);
+    printf("Enter second integer number: ");
+    scanf("%d", &number_5);
 
     sum = number_1 + number_2 + number_3; // sum calculation
-    average = sum / 3; // average 
+    average = sum / 3; // average calculation
     percentage = (number_1 / number_2) * 100; // percentage calculation
+    division = number_4 / number_5; // division calculation
+    remainder = number_4 % number_5; // remainder calculation
 
     // print of the results:
     printf("The sum of the three floating-point numbers is %.3f.", sum);
     printf("\nThe average of the three floating-point numbers is %.3f.", average);
     printf("\nThe first number is %.1f%% of the second number.", percentage);
+    printf("\nThe division of the first number by the second number is %d, and the remainder is %d.", division, remainder);
 
     return 0;
 }
