579938a3e7186603865912b733312f266b047711
Lívia Guimarães <C24329646@myTUDublin.ie>
2026-04-10 20:43:39 +0100

    Conditional statements

---
+++ Programming_lab4/question1.c
@@ -0,0 +1,33 @@
+/* Program description: inputung characters from standard-input and displaying a message whether the character is a vowel or not.
+Important to check both upper and lower case characters, and ensure to use a switch statement.
+Author: Lívia Ferreira Guimarães Neves */
+#include <stdio.h>
+
+int main()
+{
+    char c;
+
+    printf("Enter a character: ");
+    scanf("%c", &c);
+
+    switch (c)
+    {
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+        case 'A':
+        case 'E':
+        case 'I':
+        case 'O':
+        case 'U':
+            printf("%c is a vowel.\n", c);
+            break;
+        default:
+            printf("%c is not a vowel.\n", c);
+            break;
+    }
+
+    return 0;
+}
