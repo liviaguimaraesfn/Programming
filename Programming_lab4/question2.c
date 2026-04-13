1dd2995423d1818078f33fc575c8f1300b9b0ea2
Lívia Guimarães <liviafgn31@gmail.com>
2026-04-13 23:54:41 +0100

    Conditional statements

--- Programming_lab4/question2.c
+++ Programming_lab4/question2.c
@@ -1,1 +1,29 @@
+/* Program description: calculating if an integer, between 1 and 100, is even or odd, and displaying a message on the screen.
+Author: Lívia Ferreira Guimarães Neves */
 
+#include <stdio.h>
+
+int main()
+{
+    int number;
+
+    printf("Enter an integer between 1 and 100: ");
+    scanf("%d", &number);
+
+    if (number < 1 || number > 100) // || means "or"
+    {
+        printf("Invalid input. Please enter an integer between 1 and 100.\n");
+        return 1; // exiting program
+    }
+
+    if (number % 2 == 0)
+    {
+        printf("Number %d is even.\n", number);
+    }
+    else
+    {
+        printf("Number %d is odd.\n", number);
+    }
+
+    return 0;
+}
