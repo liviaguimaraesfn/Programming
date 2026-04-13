0da32f000b6f89e07c444845c4dc63a0af1c063b
Lívia Guimarães <liviafgn31@gmail.com>
2026-04-14 00:48:58 +0100

    Conditional statements

--- Programming_lab4/question2.c
+++ Programming_lab4/question2.c
@@ -1,29 +1,52 @@
-/* Program description: calculating if an integer, between 1 and 100, is even or odd, and displaying a message on the screen.
+/* Program description: calculating if specific integers are even or odd, if the first one is divisible by the second one, finding the smallest using conditional operator "?", and displaying a message on the screen.
 Author: Lívia Ferreira Guimarães Neves */
 
 #include <stdio.h>
 
 int main()
 {
-    int number;
+    int num1, num2, smallest;
 
     printf("Enter an integer between 1 and 100: ");
-    scanf("%d", &number);
+    scanf("%d", &num1);
 
-    if (number < 1 || number > 100) // || means "or"
+    if (num1 < 1 || num1 > 100) // || means "or"
     {
         printf("Invalid input. Please enter an integer between 1 and 100.\n");
         return 1; // exiting program
     }
 
-    if (number % 2 == 0)
+    if (num1 % 2 == 0)
     {
-        printf("Number %d is even.\n", number);
+        printf("Number %d is even.\n", num1);
     }
     else
     {
-        printf("Number %d is odd.\n", number);
+        printf("Number %d is odd.\n", num1);
     }
+
+    // calculating if num1 is divisible by num2
+    printf("Enter another integer between 1 and 100: ");
+    scanf("%d", &num2);
+
+    if (num2 < 1 || num2 > 100)
+    {
+        printf("Invalid input. Please enter an integer between 1 and 100.\n");
+        return 1;
+    }
+
+    if (num1 % num2 == 0)
+    {
+        printf("Number %d is divisible by %d.\n", num1, num2);
+    }
+    else
+    {
+        printf("Number %d is not divisible by %d.\n", num1, num2);
+    }
+
+    // finding the smallest using conditional operator ?, "e.g. variable = (condition) ? value_if_true : value_if_false;"
+    smallest = (num1 < num2) ? num1 : num2; // means "if num1 is less than num2, smallest is num1, otherwise smallest is num2"
+    printf("The smallest number is: %d\n", smallest);
 
     return 0;
 }
