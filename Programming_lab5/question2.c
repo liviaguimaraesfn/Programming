b93bbfe34a2252a44d1f467edd19babc8fb7b5e9
Lívia Guimarães <liviafgn31@gmail.com>
2026-04-28 23:58:46 +0100

    processes

---
+++ Programming_lab5/question2.c
@@ -0,0 +1,34 @@
+/* Program description: reading and calculating specific processes on a positive integer based on if it is even or odd.
+Sequence based: 1. if the number is even, halve it. 2. if the number is odd, multiply by 3 and add 1
+Author: Lívia Ferreira Guimarães Neves */
+
+#include <stdio.h>
+
+int main()
+{
+    int positive_integer, counter = 0;
+
+    printf("Please enter a positive integer number: ");
+    scanf("%d", &positive_integer);
+    printf("Value entered is %d\n", positive_integer);
+
+    while (positive_integer != 1)
+    {
+        if (positive_integer % 2 == 0)
+        {
+            positive_integer = positive_integer / 2;
+            printf("Next value is %d\n", positive_integer);
+            counter = counter + 1;
+        }
+        else
+        {
+            positive_integer = (positive_integer * 3) + 1;
+            printf("Next value is %d\n", positive_integer);
+            counter = counter + 1;
+        }
+    }
+
+    printf("The steps are = %d\n", counter);
+    
+    return 0;
+}
